5d835ea3e8f6b562f3d70f8bc6758b7db286bf9b
Grey <barsynka1996@gmail.com>
2024-08-01 19:47:10 +0700

    add format script

--- vite.config.js
+++ vite.config.js
@@ -16,7 +16,7 @@ export default defineConfig({
     emptyOutDir: true,
     rollupOptions: {
       input: {
-        main: './src/index.html'
+        main: './src/index.html',
       },
     },
   },
