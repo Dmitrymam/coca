41bea7fa6e12884fdfcc286f010b95b7dedeff2f
Grey <barsynka1996@gmail.com>
2024-08-13 01:56:38 +0700

    Created hero section of audience page

---
+++ src/js/audience.js
@@ -0,0 +1,10 @@
+import '/scss/audience.scss';
+
+// component
+
+import { useTheme } from './components/theme.js';
+import { useBurger } from './components/burger.js';
+
+useTheme();
+useBurger();
+
