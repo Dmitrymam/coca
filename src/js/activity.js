eaab79f429488143cf2ec0e5bc615cec9531e6f5
Grey <barsynka1996@gmail.com>
2024-08-21 02:05:17 +0700

    Created article section of activity page

--- src/js/activity.js
+++ src/js/activity.js
@@ -4,10 +4,14 @@ import '/scss/activity.scss';
 
 import { useTheme } from './components/theme.js';
 import { useTab } from './components/activity/tab.js';
-import { useHeroActivitySlider } from './components/activity/slider.js';
+import {
+  useHeroActivitySlider,
+  useArticleActivitySlider,
+} from './components/activity/slider.js';
 import { useBurger } from './components/burger.js';
 
 useTheme();
 useTab();
 useBurger();
 useHeroActivitySlider();
+useArticleActivitySlider();
--- src/js/components/activity/slider.js
+++ src/js/components/activity/slider.js
@@ -24,3 +24,24 @@ export const useHeroActivitySlider = () => {
     },
   });
 };
+
+export const useArticleActivitySlider = () => {
+  const insightSlider = new Swiper('.article__slider', {
+    slidesPerView: 1.3,
+    spaceBetween: 10,
+    loop: true,
+    navigation: {
+      nextEl: '.article__head-btn--next',
+      prevEl: '.article__head-btn--prev',
+    },
+    scrollbar: {
+      el: '.article__scrollbar',
+    },
+    breakpoints: {
+      1201: { slidesPerView: 2.94 },
+      769: { spaceBetween: 32 },
+
+      577: { spaceBetween: 20, slidesPerView: 2 },
+    },
+  });
+};
