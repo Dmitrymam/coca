7dc299b4717adee9f59fb0fb80390dfa5aa9115d
Grey <barsynka1996@gmail.com>
2024-08-22 20:55:26 +0700

    change price calculation to hero section of pricing page

--- src/js/components/pricing/plans.js
+++ src/js/components/pricing/plans.js
@@ -3,19 +3,33 @@ export const usePlans = () => {
   const starterPrice = document.querySelector('[data-price="starter"]');
   const popularPrice = document.querySelector('[data-price="popular"]');
   const enterprisePrice = document.querySelector('[data-price="enterprise"]');
+  const starterPriceText = document.querySelector(
+    '[data-price="starter-text"]',
+  );
+  const popularPriceText = document.querySelector(
+    '[data-price="popular-text"]',
+  );
+  const enterprisePriceText = document.querySelector(
+    '[data-price="enterprise-text"]',
+  );
 
+  const swithSaleStarter = 120 * 12 * 0.8;
+  const swithSalePopular = 1680 * 12 * 0.8;
+  const swithSaleEnterprise = 2520 * 12 * 0.8;
+
+  console.log(swithSaleStarter);
   const priceList = {
     starter: {
       default: 120,
-      swithSale: 100,
+      swithSale: swithSaleStarter,
     },
     popular: {
       default: 1680,
-      swithSale: 1400,
+      swithSale: swithSalePopular,
     },
     enterprise: {
       default: 2520,
-      swithSale: 2100,
+      swithSale: swithSaleEnterprise,
     },
   };
 
@@ -23,6 +37,9 @@ export const usePlans = () => {
     starterPrice.textContent = priceList.starter.swithSale;
     popularPrice.textContent = priceList.popular.swithSale;
     enterprisePrice.textContent = priceList.enterprise.swithSale;
+    starterPriceText.textContent = '/yr';
+    popularPriceText.textContent = '/yr';
+    enterprisePriceText.textContent = '/yr';
   };
 
   switcher.checked = true;
@@ -31,6 +48,9 @@ export const usePlans = () => {
     starterPrice.textContent = priceList.starter.default;
     popularPrice.textContent = priceList.popular.default;
     enterprisePrice.textContent = priceList.enterprise.default;
+    starterPriceText.textContent = '/mo';
+    popularPriceText.textContent = '/mo';
+    enterprisePriceText.textContent = '/mo';
   };
 
   switcher.addEventListener('click', () => {
